2cf21ce2f5fe08fa0c8f52100a21ffb8358125ce
JustBugsPython <its.navneet.31@gmail.com>
2026-01-05 00:20:01 +0530

    Initial Guessing Game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
     println!("The Number Guessing Game ");
-    println!("Enter your guess ");
+    println!("Guess a number ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read uer input");
     println!("You guessed {guess}");
