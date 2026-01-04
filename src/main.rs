3e68e802cc4b508cefb3f6995abbb5f9cbb87da2
JustBugsPython <its.navneet.31@gmail.com>
2026-01-05 00:46:17 +0530

    Changed statements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 fn main() {
-    println!("The Number Guessing Game ");
+    println!("Welcome to The Number Guessing Game ");
     println!("Guess a number ");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read uer input");
